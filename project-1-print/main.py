4da778dcef6efdcba1c6d61c410ef46b74b7593a
hackdanismo <office@justtoconfirm.co.uk>
2024-06-07 11:52:38 +0100

    chore: using the print() function and manipulating strings in Python

---
+++ project-1-print/main.py
@@ -0,0 +1,22 @@
+# Run the script by opening the terminal: $ python3 main.py
+
+# Using the print() function to output what to print in the console
+# The double quotes are used to tell the computer that "Hello world!" is not code, it's a String (text)
+print("Hello world!")
+
+# Using a single print() function to output over multiple lines
+# Use the \n character
+print("Hello world!\nHello world!")
+
+# Modifiers can be added to text within the print() function
+# Using single quotes in-place of double quotes:
+print('She said: "Hello" and then left.')
+# Alternative option - escape backslash:
+print("She said: \"Hello\" and then left.")
+
+# Concatenate (combine) Strings to added to the end of another string
+# Use a + sign
+# Strings have been combined into one with no space between: HelloDan
+print("Hello" + "Dan")
+# This will add the space as a String by continuing to use String concatenation to become a single String
+print("Hello" + " " + "Dan")
